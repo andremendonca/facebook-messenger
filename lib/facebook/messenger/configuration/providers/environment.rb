06301ba18be2524b8c33b6a0b3433e408aeea1e1
André Mendonça <almend@br.ibm.com>
2017-03-21 00:49:35 -0300

    Use less generic env variable

--- lib/facebook/messenger/configuration/providers/environment.rb
+++ lib/facebook/messenger/configuration/providers/environment.rb
@@ -5,15 +5,15 @@ module Facebook
         # Configuration provider for environment variables.
         class Environment
           def valid_verify_token?(verify_token)
-            verify_token == ENV['VERIFY_TOKEN']
+            verify_token == ENV['FB_VERIFY_TOKEN']
           end
 
           def app_secret_for(*)
-            ENV['APP_SECRET']
+            ENV['FB_APP_SECRET']
           end
 
           def access_token_for(*)
-            ENV['ACCESS_TOKEN']
+            ENV['FB_ACCESS_TOKEN']
           end
         end
       end
